22a563ccf3d7b7c870c159b222a7e451613877b9
eliott <eliottbidault@gmail.com>
2021-05-22 20:54:32 +0200

    create the hover anim when the inputs are changed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-/* ---- GETTING THE VALUES AND APPLYING THE STYLE ---- */
-
 //we're getting the value of the text input.
-$("input[type='text']").keypress(function () {
+$("input[type='text']").on('keypress', function () {
     setTimeout(() => {
         //apllying the style when the user change the input[type='text'] value
-        $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
+        if (state == 'passive') {
+            $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
+        } else if (state == 'hover') {
+            //defining the initial value of the button style before the hover effect so the value is took before it changes
+            let oldVal = $("#btn").css($(this).attr('placeholder').toLowerCase());
+
+            //changing the style on hover
+            $("#btn").hover(() => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
+            }, () => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), oldVal);
+            });
+        }
+
         if ($(this).attr('placeholder').toLowerCase() == 'value') {
             $("#btn").val($(this).val());
         }
@@ -12,37 +23,46 @@ $("input[type='text']").keypress(function () {
 });
 
 //we're getting the value of the ranges
-$("input[type='range']").mouseup(() => {
-    if (group == 'bordersAndOutline') {
-        setTimeout(() => {
-            //we change the style when the user change the input[type='range'] value
+$("input[type='range']").on('mouseup', function () {
+    //applying the style when the user release his mouse on input[type='range']
+    setTimeout(() => {
+        if (state == 'passive') {
             $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
-        }, 150);
-    } else {
-        setTimeout(() => {
-            //we change the style when the user change the input[type='range'] value
-            $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
-        }, 150);
-    }
+        } else if (state == 'hover') {
+            //defining the inital value of the button style before the hover effect so the value is took before it changes
+            let oldVal = $("#btn").css($(this).attr('placeholder').toLowerCase());
 
+            //changing the style on hover
+            $("#btn").hover(() => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
+            }, () => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), oldVal);
+            });
+        }
+    }, 150);
 });
 
 //we're getting the value of the select's options
-$('select').click(function () {
-    if (group == 'bordersAndOutline') {
-        //we loop through the 4 divs of the borders and outline div
-        for (let i = 0; i < $(`.${group}`).eq(groupIndex).children().length; i++) {
-            //we loop through the inputs childs of the 4 divs
-            for (let j = 0; j < $(`.${group}`).eq(groupIndex).children().eq(i).children().length; j++) {
-                setTimeout(() => {
-                    $("#btn").css($(this).attr('aria-placeholder'), $(this).val());
-                }, 150);
+$('select').on('click', function () {
+    if (group == 'border') {
+        setTimeout(() => {
+            if (state == 'passive') {
+                $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), $(this).val());
+            } else if (state == 'hover') {
+                //defining the initial value of the button style before the gover effect so the value is took before ut changes
+                let oldVal = $("#btn").css($(this).attr('aria-placeholder').toLowerCase());
+
+                //changing the style on hover
+                $("#btn").hover(() => {
+                    $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), $(this).val());
+                }, () => {
+                    $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), oldVal);
+                });
             }
-        }
+        }, 150);
     } else if (group == 'colors') {
         for (let i = 0; i < $(`.${group}`).eq(groupIndex).children().length; i++) {
             setTimeout(() => {
-                console.log($(`.${group}`).eq(groupIndex).children().eq(i).val());
                 if ($(`.${group}`).eq(groupIndex).children().eq(i).val() == 'linear-gradient'
                     || $(`.${group}`).eq(groupIndex).children().eq(i).val() == 'radial-gradient') {
                     //append the right content to create a liner or radial gradient
@@ -51,5 +71,3 @@ $('select').click(function () {
         }
     }
 });
-
-/* ---- END OF GROUP ---- */
