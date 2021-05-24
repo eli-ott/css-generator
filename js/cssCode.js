f62307ebb732d44afe64a0af7f9592aea8d3bf45
eliott <eliottbidault@gmail.com>
2021-05-24 17:25:59 +0200

    created the css generator code and fix bugs

--- js/cssCode.js
+++ js/cssCode.js
@@ -1,10 +1,60 @@
-let stylesValue = [];
+let passiveStylesAttribute = [];
+let passiveStylesValue = [];
+let passiveCSSCode = [];
 
-const generateCode = () => {
-    for (let i = 0; i < changedStyle.length; i++) {
-        console.log(changedStyle[i]);
-        stylesValue[i] = $(`#${changedStyle[i]}`).val();
+let hoverStylesAttribute = [];
+let hoverStylesValue = [];
+let hoverCSSCode = [];
+
+let clickStylesAttribute = [];
+let clickStylesValue = [];
+let clickCSSCode = [];
+
+const generateValues = () => {
+    //we get the value of the modified passive inputs and we push them inside an array
+    for (let i = 0; i < $("#passive").find("[changed='true']").length; i++) {
+        passiveStylesAttribute[i] = $("#passive").find("[changed='true']").eq(i).attr('placeholder');
+        passiveStylesValue[i] = $("#passive").find("[changed='true']").eq(i).val();
     }
+    for (let i = 0; i < $("#hover").find("[changed='true']").length; i++) {
+        hoverStylesAttribute[i] = $("#hover").find("[changed='true']").eq(i).attr('placeholder');
+        hoverStylesValue[i] = $("#hover").find("[changed='true']").eq(i).val();
+    }
+    for (let i = 0; i < $("#click").find("[changed='true']").length; i++) {
+        clickStylesAttribute[i] = $("#click").find("[changed='true']").eq(i).attr('placeholder');
+        clickStylesValue[i] = $("#click").find("[changed='true']").eq(i).val();
+    }
+
+    generateCode();
 }
 
-setInterval(generateCode, 1000);
+const generateCode = () => {
+    //wwe create a line of code depending on the value that we got and the name of the property
+    for (let i = 0; i < passiveStylesAttribute.length; i++) {
+        passiveCSSCode[i] = `&nbsp; <span class='attr'>${passiveStylesAttribute[i].toLowerCase()}</span><span class='punctuation'>:</span> &nbsp; <span class='attrVal'>${passiveStylesValue[i]}</span><span class='punctuation'>;</span> <br/>`;
+    }
+    for (let i = 0; i < hoverStylesAttribute.length; i++) {
+        hoverCSSCode[i] = `&nbsp; <span class='attr'>${hoverStylesAttribute[i].toLowerCase()}</span><span class='punctuation'>:</span> &nbsp; <span class='attrVal'>${hoverStylesValue[i]}</span><span class='punctuation'>;</span> <br/>`;
+    }
+    for (let i = 0; i < clickStylesAttribute.length; i++) {
+        clickCSSCode[i] = `&nbsp; <span class='attr'>${clickStylesAttribute[i].toLowerCase()}</span><span class='punctuation'>:</span> &nbsp; <span class='attrVal'>${clickStylesValue[i]}</span><span class='punctuation'>;</span> <br/>`;
+    }
+
+    //we're removing all the elements inside the code sample paragraph
+    $("#codeSample").empty();
+
+    //we're defining the code content with the right class to highlight it after
+    let finalcode = `<span class='property'>input[type='button']</span><span class='punctuation'> {</span> <br>
+        ${passiveCSSCode.join('')}
+        <span class='punctuation'>}</span> <br>
+        <span class='property'>input[type='button']</span><span class='pseudoElem'>:hover</span><span class='punctuation'> {</span> <br>
+        ${hoverCSSCode.join('')}
+        <span class='punctuation'>}</span> <br>
+        <span class='property'>input[type='button']</span><span class='pseudoElem'>:focus</span><span class='punctuation'> {</span> <br>
+        ${clickCSSCode.join('')}
+        <span class='punctuation'>}</span> <br>
+    `;
+
+    //we're appending the code we created
+    $("#codeSample").append(finalcode)
+}
--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-let changedStyle = [];
-
 //we're getting the value of the text input.
 $("input[type='text']").on('keypress', function () {
     setTimeout(() => {
-        //pushing the changed style in the changedStyle array to reuse it in the cssCode.js
-        console.log($(this).attr('class'));
-        if($(this).attr('changed') != 'true'){
-            changedStyle.push($(this).attr('id'));
-            $(this).attr('changed', 'true');
-        }
 
         //apllying the style when the user change the input[type='text'] value
         if (state == 'passive') {
+            //pushing the changed style in the passiveChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
             $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
         } else if (state == 'hover') {
+            //pushing the changed style in the hoverChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
             //defining the initial value of the button style before the hover effect so the value is took before it changes
-            let oldVal = $("#btn").css($(this).attr('placeholder').toLowerCase());
+            let oldVal = $('#passive').find(`#${$(this).attr('id')}`).val();
 
             //changing the style on hover
             $("#btn").hover(() => {
@@ -24,8 +26,13 @@ $("input[type='text']").on('keypress', function () {
                 $("#btn").css($(this).attr('placeholder').toLowerCase(), oldVal);
             });
         } else if (state == 'click') {
+            //pushing the changed style in the clickChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
             //defining the inital value of the button style before the click effect so the value is took before it changes
-            let oldVal = $("#btn").css($(this).attr('placeholder').toLowerCase());
+            let oldVal = $('#hover').find(`#${$(this).attr('id')}`).val();
 
             //we change the style when the user is clicking the button
             $("#btn").mousedown(() => {
@@ -48,10 +55,20 @@ $("input[type='range']").on('mouseup', function () {
     //applying the style when the user release his mouse on input[type='range']
     setTimeout(() => {
         if (state == 'passive') {
+            //pushing the changed style in the passiveChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
             $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
         } else if (state == 'hover') {
+            //pushing the changed style in the hoverChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
             //defining the inital value of the button style before the hover effect so the value is took before it changes
-            let oldVal = $("#btn").css($(this).attr('placeholder').toLowerCase());
+            let oldVal = $('#passive').find(`#${$(this).attr('id')}`).val();
 
             //changing the style on hover
             $("#btn").hover(() => {
@@ -60,8 +77,13 @@ $("input[type='range']").on('mouseup', function () {
                 $("#btn").css($(this).attr('placeholder').toLowerCase(), oldVal);
             });
         } else if (state == 'click') {
+            //pushing the changed style in the clickChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
             //defining the inital value of the button style before the hover effect so the value is took before it changes
-            let oldVal = $("#btn").css($(this).attr('placeholder').toLowerCase());
+            let oldVal = $('#hover').find(`#${$(this).attr('id')}`).val();
 
             //we change the style when the user is clicking the button
             $("#btn").mousedown(() => {
@@ -77,42 +99,46 @@ $("input[type='range']").on('mouseup', function () {
 
 //we're getting the value of the select's options
 $('select').on('click', function () {
-    if (group == 'borders') {
-        setTimeout(() => {
-            if (state == 'passive') {
-                $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), $(this).val().toLowerCase());
-            } else if (state == 'hover') {
-                //defining the initial value of the button style before the gover effect so the value is took before ut changes
-                let oldVal = $("#btn").css($(this).attr('aria-placeholder').toLowerCase());
-
-                //changing the style on hover
-                $("#btn").hover(() => {
-                    $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), $(this).val());
-                }, () => {
-                    $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), oldVal);
-                });
-            } else if (state == 'click') {
-                //defining the inital value of the button style before the hover effect so the value is took before it changes
-                let oldVal = $("#btn").css($(this).attr('aria-placeholder').toLowerCase());
-
-                //we change the style when the user is clicking the button
-                $("#btn").mousedown(() => {
-                    $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), $(this).val());
-                });
-                //we change the style when the user releasing the click on the button
-                $("#btn").mouseup(() => {
-                    $("#btn").css($(this).attr('aria-placeholder').toLowerCase(), oldVal);
-                });
+    setTimeout(() => {
+        if (state == 'passive') {
+            //pushing the changed style in the passiveChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
             }
-        }, 150);
-    } else if (group == 'colors') {
-        for (let i = 0; i < $(`.${group}`).eq(groupIndex).children().length; i++) {
-            setTimeout(() => {
-                if ($(`.${group}`).eq(groupIndex).children().eq(i).val() == 'linear-gradient'
-                    || $(`.${group}`).eq(groupIndex).children().eq(i).val() == 'radial-gradient') {
-                    //append the right content to create a liner or radial gradient
-                }
-            }, 150);
+
+            $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val().toLowerCase());
+        } else if (state == 'hover') {
+            //pushing the changed style in the hoverChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
+            //defining the initial value of the button style before the gover effect so the value is took before ut changes
+            let oldVal = $('#passive').find(`#${$(this).attr('id')}`).val();
+
+            //changing the style on hover
+            $("#btn").hover(() => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
+            }, () => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), oldVal);
+            });
+        } else if (state == 'click') {
+            //pushing the changed style in the clickChangedStyle array to reuse it in the cssCode.js
+            if ($(this).attr('changed') != 'true') {
+                $(this).attr('changed', 'true');
+            }
+
+            //defining the inital value of the button style before the hover effect so the value is took before it changes
+            let oldVal = $('#hover').find(`#${$(this).attr('id')}`).val();
+
+            //we change the style when the user is clicking the button
+            $("#btn").mousedown(() => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), $(this).val());
+            });
+            //we change the style when the user releasing the click on the button
+            $("#btn").mouseup(() => {
+                $("#btn").css($(this).attr('placeholder').toLowerCase(), oldVal);
+            });
         }
-    }
+    }, 150);
 });
